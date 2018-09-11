1187e2ac377d76455e6af581fddfc2c1dd192a12
anikaator <37859709+anikaator@users.noreply.github.com>
2018-09-11 08:31:31 -0700

    Create main.cpp

---
+++ CrackingCodingInterview/sol4.4/main.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL)
+            return true;
+        int cntLeft = checkHeight(root->left);
+        int cntRight = checkHeight(root->right);
+        if (cntLeft == -1 || cntRight == -1)
+            return false;
+        if(abs(cntLeft - cntRight) > 1)
+            return false;
+        else 
+            return true;
+    }
+    
+    int checkHeight(struct TreeNode* node) {
+        if (node == NULL)
+            return 0;
+        if (node->left == NULL && node->right == NULL)
+            return 1;
+        int cntLeft = checkHeight(node->left);
+        int cntRight = checkHeight(node->right);
+        if (cntLeft == -1 || cntRight == -1)
+            return -1;
+        if(abs(cntLeft - cntRight) > 1)
+            return -1;
+        else 
+            return cntLeft > cntRight ? cntLeft+1 : cntRight+1;        
+    }
+};
