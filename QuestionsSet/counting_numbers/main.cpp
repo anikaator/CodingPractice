ebcdbf14111ccd048b32b25616e9e575e32ce587
anikaator <37859709+anikaator@users.noreply.github.com>
2018-11-02 08:30:17 -0700

    Create main.cpp

---
+++ QuestionsSet/counting_numbers/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ret;
+        ret.push_back(0);
+        for (int i=1; i<=num; i++) {
+            ret.push_back(ret[i&i-1] + 1); 
+        }
+        return ret;
+    }
+};
